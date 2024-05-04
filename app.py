9b35754fa12279cd704d83c8a9b67cc935a64b9f
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-05-03 23:58:05 -0300

    a

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ with tab3:
         
             st.success("Atendimento Excluído Com Sucesso!")
 
-    opcoes = opcoes.set_index("Paciente",inplace=True)
+    opcoes = opcoes.set_index("Hora",inplace=True)
     st.dataframe(opcoes,use_container_width=True)
 
 #---------------------------------------------------------------------------------------------------------------------
