02a9840340e6cb0bfde7d0b3be07b4e57a5e48de
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-05-05 14:15:52 -0300

    a

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ with tab1:
         if status == "Ok":
             return "Agendado"
         elif status == "Atendido":
-            pass
+            status == "Atendido"
         
     df["Status"] = df.apply(lambda row: definir_status(row["Status"]), axis= 1)
     st.table(df)
