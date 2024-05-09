5284b1c89f44627ee06ec9e5c3f14ab65753506b
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-05-08 23:10:32 -0300

    a

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ st.markdown(hidecontainerdataframe,unsafe_allow_html=True)
 
 containertab2 = """
     <style>
-    [id="tabs-bui4-tabpanel-1"]
+    [id="tabs-bui4-tabpanel"]
     {
     background-color: #0C2C2C;
     padding: 20px;
